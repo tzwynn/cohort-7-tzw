ebb4ffa72817fb5f86caabd6f28352cbe034716e
tzwynn <thaw_wynn@yahoo.co.uk>
2024-09-21 23:15:42 +0100

    main.tf to aws-secret-engine-dynamic-role folder

---
+++ aws-secret-engine-dynamic-role/main.tf
@@ -0,0 +1,24 @@
+#Enabling secret engine in vault and configuering root
+resource "vault_aws_secret_backend" "aws" {
+  access_key = aws_iam_access_key.vault-admin-key.id
+  secret_key = aws_iam_access_key.vault-admin-key.secret
+  region = "eu-west-2"
+  path = "aws-master"
+  default_lease_ttl_seconds = 900
+  max_lease_ttl_seconds = 1500
+}
+
+#Creating dynamic admin role
+resource "vault_aws_secret_backend_role" "admin-access-role" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "admin-access-role"
+  credential_type = "iam_user"
+  policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+
+}
+
+#Retriving the role data
+data "vault_aws_access_credentials" "creds" {
+  backend = vault_aws_secret_backend.aws.path
+  role    = vault_aws_secret_backend_role.admin-access-role.name
+}
---
+++ aws-secret-engine-dynamic-role/output.tf
@@ -0,0 +1,10 @@
+output "access-kye"{
+    value = data.vault_aws_access_credentials.creds.access_key
+    sensitive = true
+}
+
+output "secret_key" {
+    value = data.vault_aws_access_credentials.creds.secret_key
+    sensitive = true
+  
+}
