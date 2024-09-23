b17828942eb871079b8f5165d27b493f48f6711b
tzwynn <thaw_wynn@yahoo.co.uk>
2024-09-23 21:50:42 +0100

    adding vpc-day3 folder with .tf files

---
+++ vpc-day3/main.tf
@@ -0,0 +1,94 @@
+resource "aws_vpc" "c7-vault-vpc" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "c7-vault-vpc"
+  }
+}
+
+resource "aws_subnet" "c7-public-subnet" {
+  count = length(var.public-cidr-block)
+  vpc_id     = aws_vpc.c7-vault-vpc.id
+  cidr_block = var.public-cidr-block[count.index]
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "c7-public-subnet-${element(data.aws_availability_zones.azs.names, count.index)}"
+  }
+}
+
+resource "aws_route_table" "c7-public-rt" {
+  vpc_id = aws_vpc.c7-vault-vpc.id
+  tags = {
+    Name = "c7-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public-cidr-block)
+  subnet_id      = aws_subnet.c7-public-subnet[count.index].id
+  route_table_id = aws_route_table.c7-public-rt
+}
+
+resource "aws_internet_gateway" "c7-public-gw" {
+  vpc_id = aws_vpc.c7-vault-vpc.id
+
+  tags = {
+    Name = "c7-public-gw"
+  }
+}
+
+resource "aws_route" "c7-route" {
+  route_table_id            = aws_route_table.c7-public-rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.c7-public-gw.id
+}
+
+#private subnet
+
+resource "aws_subnet" "c7-private-subnet" {
+  count = length(var.private-cidr-block)
+  vpc_id     = aws_vpc.c7-vault-vpc.id
+  cidr_block = var.private-cidr-block[count.index]
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+
+  tags = {
+    Name = "c7-private-subnet-${element(data.aws_availability_zones.azs.names, count.index)}"
+  }
+}
+
+resource "aws_route_table" "c7-private-rt" {
+  vpc_id = aws_vpc.c7-vault-vpc.id
+  tags = {
+    Name = "c7-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private-cidr-block)
+  subnet_id      = aws_subnet.c7-private-subnet[count.index].id
+  route_table_id = aws_route_table.c7-private-rt.id
+}
+
+resource "aws_eip" "c7-eip" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "c7-nat-gw" {
+  allocation_id = aws_eip.c7-eip.id
+  subnet_id     = aws_subnet.c7-public-subnet[0].id
+
+  tags = {
+    Name = "c7-nat-gw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.c7-public-gw]
+}
+
+resource "aws_route" "c7-private-route" {
+  route_table_id            = aws_route_table.c7-private-rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.c7-nat-gw.id
+}
---
+++ vpc-day3/variables.tf
@@ -0,0 +1,7 @@
+variable "public-cidr-block" {
+  default = ["10.10.1.0/24","10.10.2.0/24","10.10.3.0/24"]
+}
+
+variable "private-cidr-block" {
+  default = ["10.10.4.0/24","10.10.5.0/24","10.10.6.0/24"]
+}
