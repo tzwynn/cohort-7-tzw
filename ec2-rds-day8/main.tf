25c590b2c13c85287201148f388f3c31f6fd04cf
tzwynn <thaw_wynn@yahoo.co.uk>
2024-11-06 23:06:19 +0000

    amend db password

--- ec2-rds-day8/main.tf
+++ ec2-rds-day8/main.tf
@@ -194,7 +194,7 @@ resource "aws_db_instance" "project_rds" {
   identifier             = "db-instance"
   instance_class         = "db.t3.micro"
   username               = "admin"
-  password               = "admin"
+  password               = "cohort7proj"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.allow_db_port.id]
 }
