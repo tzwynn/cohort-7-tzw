dea5faaa2676768e18edad8799c70e7016e9b9ab
tzwynn <thaw_wynn@yahoo.co.uk>
2024-11-08 23:57:37 +0000

    fix error in main.tf

--- database-secret-engine-day10/main.tf
+++ database-secret-engine-day10/main.tf
@@ -7,16 +7,17 @@ resource "vault_database_secrets_mount" "db" {
     username       = "vault"
     password       = "vault"
     connection_url = "{{username}}:{{password}}@tcp(${data.terraform_remote_state.ec2-rds-day8.outputs.rds_hostname}:3306)"
-    allowed_roles = ["*"]
+    allowed_roles  = ["*"]
   }
+}
 
 #Creating a role 
 resource "vault_database_secret_backend_role" "readwrite_role" {
-  name    = "readwrite-role"
-  backend = vault_database_secrets_mount.db.path
-  db_name = vault_database_secrets_mount.db.mysql[0].name
-  default_ttl =600
-  max_ttl =900
+  name        = "readwrite-role"
+  backend     = vault_database_secrets_mount.db.path
+  db_name     = vault_database_secrets_mount.db.mysql[0].name
+  default_ttl = 600
+  max_ttl     = 900
   creation_statements = [
     "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';",
     "GRANT ALL PRIVILEGES ON projectdb.* TO '{{name}}'@'%';"
@@ -24,11 +25,11 @@ resource "vault_database_secret_backend_role" "readwrite_role" {
 }
 
 resource "vault_database_secret_backend_role" "readonly_role" {
-   name    = "readonly-role"
-  backend = vault_database_secrets_mount.db.path
-  db_name = vault_database_secrets_mount.db.mysql[0].name
-  default_ttl =600
-  max_ttl =900
+  name        = "readonly-role"
+  backend     = vault_database_secrets_mount.db.path
+  db_name     = vault_database_secrets_mount.db.mysql[0].name
+  default_ttl = 600
+  max_ttl     = 900
   creation_statements = [
     "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';",
     "GRANT SELECT ON projectdb.* TO '{{name}}'@'%';"
